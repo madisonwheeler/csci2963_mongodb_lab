e3300e7ba4b2f018151f585f9efb5424e6887d0d
Madison Wheeler <madisonawheeler@gmail.com>
2017-04-07 12:06:59 -0400

    Update checkpoint4.py

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,5 +1,31 @@
 from pymongo import MongoClient
-client = MongoClient()
+import bson
+from bson.objectid import ObjectId
 
-if __name__ == '__main__':
-    print "Modify me"
+client = MongoClient()
+db = client.csci2963
+defs = db.definitions
+
+print "All records"
+for post in defs.find():
+  print post
+
+print "\nOne record"
+doc = defs.find_one()
+print doc
+
+print "\nSpecific record"
+doc = defs.find_one({"word": "Capitaland"})
+print doc
+
+print "\nSpecific ID"
+def get(post_id):
+  document = defs.find_one({'_id': ObjectId(post_id)})
+  return document
+
+doc = get("56fe9e22bad6b23cde07b8ce")
+print doc
+
+print "\nInsert"
+post_id = defs.insert_one({"word": "Flugelhorn", "definition": "instrument"})
+print post_id
